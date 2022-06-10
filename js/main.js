a0c33e4ca1b811460815304a0e19795c5c37f6eb
Catalin Grigoriev <catalingrigoriev50@gmail.com>
2022-06-10 12:32:59 +0300

    release

--- js/main.js
+++ js/main.js
@@ -5,3 +5,26 @@ window.onscroll = function() {
     	document.getElementById("hds").classList.remove("hdBackground");
     }
 }
+
+function scrollBottom() {
+    window.scrollTo(0, document.querySelector("#donate").scrollHeight+8);
+}
+
+function showIP(){
+    document.getElementById("copyIP").style.display = "block";
+}
+
+function hideIP(){
+    document.getElementById("copyIP").style.display = "none";
+}
+
+let server_name = "inecraft Server Name"
+let temp = 0;
+function typeWriter() {
+  if (temp < server_name.length) {
+    document.getElementById("servername").innerHTML += server_name.charAt(temp);
+    temp++;
+  }
+}
+
+setInterval(typeWriter, 50);
